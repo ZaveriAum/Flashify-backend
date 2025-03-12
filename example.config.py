12946151e8ecad05480815a9856159877b75c882
Aum Zaveri <aumzaveri06@gmail.com>
2025-03-12 13:13:09 -0400

    example.config updated

--- example.config.py
+++ example.config.py
@@ -2,4 +2,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///your/path/Flashify.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     OPENAI_KEY = "your open ai secret key"
-    JWT_SECRET_KEY="your jwt secret"
+    ACCESS_TOKEN_SECRET_KEY="your-secret-key"
+    REFRESH_TOKEN_SECRET_KEY="your-secret-key"
+
